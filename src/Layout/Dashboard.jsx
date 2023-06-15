44b300342b0b0fa4ac966bfa2d60b2d0b70dab57
Joshim-Uddin <mju881@gmail.com>
2023-06-15 22:40:50 +0600

    role based dashboard added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProviders";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
-  const isAdmin = false;
-  const isInstructor = true;
-  const options = isAdmin ? (
-    <>
-      <li>
-        <Link to="classes">Manage Classes</Link>
-      </li>
-      <li>
-        <Link to="users">Manage Users</Link>
-      </li>
-    </>
-  ) : isInstructor ? (
-    <>
-      <li>
-        <Link to="addclass">Add a Class</Link>
-      </li>
-      <li>
-        <Link to="myclass">My Classes</Link>
-      </li>
-      <li>
-        <Link to="myclass">Enrolled Students</Link>
-      </li>
-      <li>
-        <Link to="myclass">Feedback</Link>
-      </li>
-    </>
-  ) : (
-    <>
-      <li>
-        <Link to="classes">My Classes</Link>
-      </li>
-      <li>
-        <Link to="users">Modify Classes</Link>
-      </li>
-    </>
-  );
+  const [role, setRole] = useState("user");
+  useEffect(() => {
+    fetch(`http://localhost:5000/user?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setRole(data.role));
+  }, [user]);
+  console.log(role);
+  const options =
+    role === "admin" ? (
+      <>
+        <li>
+          <Link to="classes">Manage Classes</Link>
+        </li>
+        <li>
+          <Link to="users">Manage Users</Link>
+        </li>
+      </>
+    ) : role === "instructor" ? (
+      <>
+        <li>
+          <Link to="addclass">Add a Class</Link>
+        </li>
+        <li>
+          <Link to="myclass">My Classes</Link>
+        </li>
+        <li>
+          <Link to="myclass">Enrolled Students</Link>
+        </li>
+        <li>
+          <Link to="myclass">Feedback</Link>
+        </li>
+      </>
+    ) : (
+      <>
+        <li>
+          <Link to="classes">My Classes</Link>
+        </li>
+        <li>
+          <Link to="users">Modify Classes</Link>
+        </li>
+      </>
+    );
 
   const fixedOptions = (
     <>
@@ -68,10 +74,10 @@ const Dashboard = () => {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content flex flex-col gap-2">
+        <ul className="menu p-4 w-80 h-full bg-indigo-400 text-white flex flex-col gap-2">
           {/* Sidebar content here */}
           {options}
-          <div className="divider"></div>
+          <div className="divider bg-white h-[2px]"></div>
           {fixedOptions}
         </ul>
       </div>
